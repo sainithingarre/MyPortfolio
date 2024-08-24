29a15b4cff3f6913b96d7f93cee76dec51215edf
sainithingarre <115415466+sainithingarre@users.noreply.github.com>
2024-08-24 14:08:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>FrontEnd Developer</i>', ' <i>MERN Developer</i>', ' <i>Competitive Programmer</i>'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
